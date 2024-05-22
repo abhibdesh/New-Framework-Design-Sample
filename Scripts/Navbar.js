cf3c3348a44d9129553a62de9a1e2b4a9a437002
Abhilasha Deshmukh <88450158+abhibdesh@users.noreply.github.com>
2024-05-22 15:43:14 +0530

    Navbar

---
+++ Scripts/Navbar.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#menu-icon').click(function() {
+        $('#nav-menu').toggleClass('active');
+    });
+
+    $('.dropdown > a').click(function(event) {
+        event.preventDefault();
+        $(this).parent().toggleClass('active');
+    });
+});
