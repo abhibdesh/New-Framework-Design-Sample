d477ce4d1df7f38fabc5c837e0df76ea880e9039
Abhilasha Deshmukh <88450158+abhibdesh@users.noreply.github.com>
2024-05-28 18:21:37 +0530

    cards and navbar change

--- Scripts/Navbar.js
+++ Scripts/Navbar.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
    
+const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yArray = [55, 49, 44, 24, 15];
 
+
+const layout = {title:"Member Status"};
+
+const data = [{labels:xArray, values:yArray, type:"pie", marker: {
+    colors: ["#FF5733", "#33FF57", "#3357FF", "#F3FF33", "#FF33F3"] // Custom colors
+}}];
+
+Plotly.newPlot("myChart", data, layout);
+    
    
 });
 
